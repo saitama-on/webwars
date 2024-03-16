df40a12c3d37329c26615841363442b620aa51d0
Shashank <shm2942004@gmail.com>
2024-03-16 21:22:01 +0530

    Login Page

---
+++ home.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var productDiv = document.getElementById("text-image");
+    productDiv.addEventListener("click", function() {
+        window.location.href = "feature.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var productDiv = document.getElementById("home");
+    productDiv.addEventListener("click", function() {
+        window.location.href = "home.html";
+    });
+});
